d41bd70268fd5ed6f903b18498a1e0358cf35644
Philippa Wisniewski <pecfwisniewski@gmail.com>
2019-11-21 19:08:23 +0000

    Initial site

---
+++ src/components/Articles.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="section">
+    <ul class="article-list">
+      <h1>{{$static.header.edges[0].node.name}}</h1>
+      <img :src="`${$static.header.edges[0].node.image.file.url.src}`" />
+      <h2>{{$static.header.edges[0].node.specialism}}</h2>
+      <div>{{$static.header.edges[0].node.location}}</div>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+
+}
+</style>
+
+<static-query>
+  query HomeQuery {
+    posts: allContentfulBlogPost {
+      edges {
+        node {
+          title
+          description
+          slug
+          publishDate(format: "MMMM Do, YYYY")
+          tags
+          heroImage {
+            path
+            slug
+            file {
+              fileName
+              url
+            }
+          }
+          description
+        }
+      }
+    }
+    header: allContentfulPageHeader {
+      edges {
+        node {
+          image {
+            path
+            slug
+            file {
+              fileName
+              url
+            }
+          }
+          name
+          specialism
+          location
+        }
+      }
+    }
+  }
+</static-query>
+<script>
+
+export default {
+  components: {
+  },
+};
+</script>
